4e6295158d7b159d78110632e9c116521dda928e
Alejandro Pla <aleplacambra@gmail.com>
2022-09-17 20:09:35 +0200

    Añadiendo estilos a la vista de 404

--- client/src/features/notFoundPage/notFoundPage.tsx
+++ client/src/features/notFoundPage/notFoundPage.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { history } from '../../helpers/history';
+import { Box, Button, Center, Flex, FormControl, FormHelperText } from '@chakra-ui/react';
 
 /**
  *
  * @returns
  */
 export const NotFoundPage = () => {
+    const onSubmit = () => history.push('/');
+
     return (
-        <div>
-            <p>not found</p>
-        </div>
+        <Box>
+            <Center h={['', '100vh']}>
+                <Flex w="full" align="center" justifyContent="center">
+                    <Box>
+                        <FormControl>
+                            <Center my={20} textAlign="center">
+                                <FormHelperText fontSize={['4xl', '6xl']} color="black">
+                                    404 - PAGE NOT FOUND
+                                </FormHelperText>
+                            </Center>
+
+                            <Center my={20}>
+                                <Button type="submit" textTransform="uppercase" colorScheme="black" background="black" color="white" onClick={onSubmit}>
+                                    Go back
+                                </Button>
+                            </Center>
+                        </FormControl>
+                    </Box>
+                </Flex>
+            </Center>
+        </Box>
     );
 };
