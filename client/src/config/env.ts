2e62d802be6dcf1b6f219e136259bea8ccf12959
Alejandro Pla <aleplacambra@gmail.com>
2022-09-22 13:46:14 +0200

    Configurando puertos para el deploy en pro

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -1,13 +1,12 @@
 const env = {
     development: {
-        BASE_URL: 'http://localhost:4000/',
+        BASE_URL: 'http://localhost:' + process.env.REACT_APP_PORT + '/',
     },
     production: {
-        BASE_URL: 'http://localhost:4000/',
+        BASE_URL: 'http://51.178.18.68:' + process.env.REACT_APP_PORT + '/',
     },
     test: {
-        BASE_URL: 'http://localhost:4000/',
+        BASE_URL: 'http://localhost:' + process.env.REACT_APP_PORT + '/',
     },
 };
-
-export const env_var = env[process.env.NODE_ENV];
+export const env_var = env[process.env.REACT_APP_NODE_ENV as keyof typeof env];
